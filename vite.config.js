9093cb4472e38038a2eaf750632fdee0bad05cc9
KagoBoy <yanvictor06@hotmail.com>
2025-05-04 18:30:06 -0300

    S

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
@@ -13,11 +8,7 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist',
-    rollupOptions:{
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      }
-    }
+    emptyOutDir: true
   },
   base: './',
   server:{
