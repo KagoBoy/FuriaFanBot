6149cf283ce779d3a426b6ec7876b6bd081f0786
KagoBoy <yanvictor06@hotmail.com>
2025-05-04 09:53:55 -0300

    atualizacao server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ import cors from 'cors';
 import axios from 'axios';
 
 const app = express();
-const PORT = 3001;
 const PANDASCORE_TOKEN = 'qEhE7ZpYUoddzxd-s3KpGL-EyBoj1nmRcwEjJXCLQiMVuE408-M';
 
 // Configuração básica do CORS para permitir requisições do frontend
 app.use(cors({
-  origin: 'https://furia-fan-bot-ebon.vercel.app/' // Permite apenas requisições do seu frontend
+  origin: ['https://furia-fan-bot-ebon.vercel.app/', 'http://localhost:3001']
 }));
 
 // Rota para obter os dados das partidas
@@ -42,6 +41,4 @@ app.get('/api/upcoming-matches', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor backend rodando em http://localhost:${PORT}`);
-});
+export default app;
